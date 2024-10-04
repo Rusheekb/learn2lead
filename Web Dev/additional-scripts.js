a05d1eba7b1168a545b184d7053c29174d91e919
Rusheek <69225274+Rusheekb@users.noreply.github.com>
2024-10-03 21:36:10 -0500

    Delete Web Dev directory

--- Web Dev/additional-scripts.js
+++
@@ -1,6 +0,0 @@
-// Example of additional JavaScript functionality
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Additional scripts loaded");
-
-    // Add any additional JavaScript functionality here
-});
